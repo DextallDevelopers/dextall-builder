d093ade0e64103868cea5ae86f8243c59e9fafa6
Гныбиденко Валерий <valeriy.gnybidenko@gmail.com>
2022-11-23 11:25:29 +0200

    feature: complete login page: 2h

--- components/Login.vue
+++ components/Login.vue
@@ -1,5 +1,73 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const $inputs = ref([])
+const formData = reactive({
+  hasErrors: true,
+  inputs: [
+    {
+      required: true,
+      id: 'name',
+      name: 'Name',
+      label: 'YOUR NAME',
+      type: 'text',
+      validation: 'required',
+      validationText: 'field cannot be empty',
+      error: true,
+      value: '',
+    },
+    {
+      required: true,
+      id: 'email',
+      name: 'Email',
+      label: 'YOUR EMAIL',
+      type: 'email',
+      validation: 'email',
+      validationText: 'incorrect email',
+      error: true,
+      value: '',
+    },
+  ],
+})
+
+const { onInputValue, onSubmit } = useForm(formData, $inputs, 'Dextall login')
+</script>
 
 <template>
-  <div>Hello World</div>
+  <section class="section section--nm login">
+    <div class="container login__wrapper">
+      <div class="login__main-block">
+        <h1 class="login__h1">151 Friendship Street</h1>
+        <form class="login__form" novalidate @submit.prevent="onSubmit">
+          <legend class="login__title">
+            Enter your email address below to access the proposal:
+          </legend>
+          <div class="login__input-wrapper">
+            <TheInput
+              v-for="input in formData.inputs"
+              :id="input.id"
+              :key="input.id"
+              ref="$inputs"
+              class="login__input"
+              :type="input.type"
+              :placeholder="input.label"
+              :required="input.required"
+              :validation-text="input.validationText && input.validationText"
+              :validation="input.validationText && input.validation"
+              @input-value="onInputValue"
+            />
+          </div>
+          <CircleButton class="login__btn">Login</CircleButton>
+        </form>
+      </div>
+      <div class="login__date-wrapper">
+        <div class="login__date">
+          <p class="login__date-text">Date of the quote:</p>
+          <p class="login__date-number">25.10.22</p>
+        </div>
+        <div class="login__date">
+          <p class="login__date-text">Expiration of the quote:</p>
+          <p class="login__date-number">25.10.22</p>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
---
+++ composables/form.ts
@@ -0,0 +1,87 @@
+import { iInputData } from '~~/composables/input'
+import { useAppStore } from '~/store/app'
+import { ToastColor } from './toasts'
+
+const URL = null
+export const useForm = (
+  formData,
+  $inputs,
+  from = 'Dextall Website',
+  URLS = []
+) => {
+  const FINAL_URLS = [URL, ...URLS]
+  const appStore = useAppStore()
+  const { addToast } = useToasts()
+
+  const emmitError = () => {
+    $inputs.value.forEach(input => input.throwError())
+  }
+
+  const resetForm = () => {
+    formData.inputs?.forEach(inp => {
+      inp.error = true
+      inp.value = ''
+    })
+
+    $inputs.value.forEach(input => input.reset())
+    formData.hasErrors = false
+  }
+
+  const onInputValue = (data: iInputData) => {
+    const idx = formData.inputs.findIndex(el => el.id === data.id)
+    formData.inputs[idx].value = data.value
+    formData.inputs[idx].error = data.error
+  }
+
+  const onSubmit = async () => {
+    return new Promise(async (resolve, reject) => {
+      const inputs = formData.inputs
+      const isError = inputs.find(el => el.error)
+
+      if (isError) {
+        emmitError()
+        return
+      }
+
+      const formSendData = new FormData()
+      formSendData.append('From:', from)
+      inputs.forEach(el => {
+        formSendData.append(el.name, el.value)
+      })
+
+      try {
+        appStore.setLoading(true)
+
+        FINAL_URLS.forEach(async url => {
+          await fetch(url, {
+            method: 'POST',
+            body: formSendData,
+            mode: 'no-cors',
+          })
+        })
+        resolve(inputs)
+        addToast({
+          color: ToastColor.success,
+          id: Date.now().toString(),
+          text: 'Form was successfully submitted',
+        })
+        resetForm()
+      } catch (error) {
+        console.log(error.message)
+        formData.hasErrors = true
+        reject(error.message)
+        addToast({
+          color: ToastColor.danger,
+          id: Date.now().toString(),
+          text: 'An error has occurred:(',
+        })
+      } finally {
+        setTimeout(() => {
+          appStore.setLoading(false)
+        }, 400)
+      }
+    })
+  }
+
+  return { onInputValue, onSubmit }
+}

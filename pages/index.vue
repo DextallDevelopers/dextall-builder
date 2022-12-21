78a3f4a88933f76b0878e4f8155ee34519616be5
letmewin22 <lkosteckiy5@gmail.com>
2022-12-21 10:25:27 +0200

    feat: add routes structure

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,3 @@
-<script setup lang="ts">
-import { useTransition } from '~/composables/transition'
-useTransition()
-
-const {isAuth, isLoaded} = useAppState()
-
-onMounted(() => {
-  const isAuthData = localStorage.getItem('isAuth')
-
-  if (isAuthData === 'true') {
-    isAuth.value = true
-  }
-  isLoaded.value = true
-})
-</script>
-
 <template>
-  <Loader v-if="!isLoaded" />
-  <div
-    v-else-if="isAuth && isLoaded"
-    data-page
-  >
-    <Main />
-    <ImagesList />
-    <Projects />
-    <News />
-    <Contacts />
-  </div>
-  <Login v-else />
+  <h1>Hello</h1>
 </template>
---
+++ pages/quotes/[name]/[version].vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useTransition } from '~/composables/transition'
+useTransition()
+
+const { isAuth, isLoaded } = useAppState()
+
+onMounted(() => {
+  const isAuthData = localStorage.getItem('isAuth')
+
+  if (isAuthData === 'true') {
+    isAuth.value = true
+  }
+  isLoaded.value = true
+})
+</script>
+
+<template>
+  <Loader v-if="!isLoaded" />
+  <div v-else-if="isAuth && isLoaded" data-page>
+    <Main />
+    <ImagesList />
+    <Projects />
+    <News />
+    <Contacts />
+  </div>
+  <Login v-else />
+</template>

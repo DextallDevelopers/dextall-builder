97ca6cd42afa64d8505d932e272a97a7b206b346
Гныбиденко Валерий <valeriy.gnybidenko@gmail.com>
2022-12-28 10:52:47 +0200

    feat: connect pricing component to storyblok: 10min

--- components/Pricing.vue
+++ components/Pricing.vue
@@ -1,19 +1,25 @@
 <script setup lang="ts">
-const { subtotalPrice, tax, totalPrice } = useComputePrice()
-const li = [
-  {
-    text: 'SUBTOTAL PRICE',
-    price: `$${subtotalPrice}/SF`,
-  },
-  {
-    text: 'TAX',
-    price: `${tax}%`,
-  },
-  {
-    text: 'TOTAL PRICE (MATERIAL ONLY)',
-    price: `$${totalPrice}`,
-  },
-]
+import { useQoutesStories } from '~/composables/stories/quotes'
+
+const route = useRoute()
+
+const { name, version } = route.params
+const { story, listenStory } = await useQoutesStories(
+  name as string,
+  version as string
+)
+
+listenStory(version)
+
+const table = computed(() => {
+  return story.value.content.product_summary_tab[0].product_summary_table[0]
+})
+
+const { totalPrice } = useComputePrice(
+  table.value.facade_area_sf[0].info,
+  table.value.subtotal_price[0].info,
+  table.value.tax[0].info
+)
 
 const { close: closeTab, tabs } = useTab()
 </script>
@@ -23,17 +29,31 @@ const { close: closeTab, tabs } = useTab()
     <div class="container pricing__wrapper">
       <h2 class="pricing__title">Pricing</h2>
       <ul class="pricing__list">
-        <li v-for="(item, idx) in li" :key="idx" class="pricing__li">
+        <li class="pricing__li">
           <div class="pricing__line"></div>
           <div class="grid pricing__text-wrapper">
-            <p class="pricing__text">
-              {{ item.text }}
-            </p>
+            <p class="pricing__text">SUBTOTAL PRICE</p>
             <p class="pricing__price">
-              {{ item.price }}
+              {{ table.subtotal_price[0].info }}
             </p>
           </div>
-          <div v-if="idx + 1 === li.length" class="pricing__line"></div>
+        </li>
+        <li class="pricing__li">
+          <div class="pricing__line"></div>
+          <div class="grid pricing__text-wrapper">
+            <p class="pricing__text">Tax</p>
+            <p class="pricing__price">
+              {{ table.tax[0].info }}
+            </p>
+          </div>
+        </li>
+        <li class="pricing__li">
+          <div class="pricing__line"></div>
+          <div class="grid pricing__text-wrapper">
+            <p class="pricing__text">TOTAL PRICE (MATERIAL ONLY)</p>
+            <p class="pricing__price">${{ totalPrice }}</p>
+          </div>
+          <div class="pricing__line"></div>
         </li>
       </ul>
       <CircleButton

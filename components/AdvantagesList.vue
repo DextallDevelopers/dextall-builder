76cdac9c21b38be50f9d895650ad7b6259052558
Гныбиденко Валерий <valeriy.gnybidenko@gmail.com>
2023-01-05 09:59:53 +0200

    feature: connect advantages list to storyblok: 20min

--- components/AdvantagesList.vue
+++ components/AdvantagesList.vue
@@ -1,40 +1,47 @@
 <script setup lang="ts">
-const li = [
-  {
-    boldText: '80% faster',
-    italicText: 'Exterior Installation',
-  },
-  {
-    boldText: '15-20% less',
-    italicText: 'Cost Lower than onsite construction',
-  },
-  {
-    boldText: '20 Times Less',
-    italicText: 'Reduce Onsite Labor',
-  },
-]
+interface iItems {
+  title_text: string
+  text: string
+}
+
+interface iProps {
+  body: {
+    content: {
+      title: string
+      description: string
+      items: iItems[]
+      image: {
+        filename: string
+      }
+    }
+  }
+}
+
+const props = defineProps<iProps>()
+
+const li = computed(() => {
+  return props.body.content.items
+})
 </script>
 
 <template>
   <section class="section section--pb advantages">
     <div class="container advantages__wrapper">
-      <h2 class="advantages__title">A New Norm in The Construction Process</h2>
+      <h2 class="advantages__title">{{ body.content.title }}</h2>
       <div class="grid advantages__content-wrapper">
         <div class="advantages__left-block">
           <p class="advantages__text">
-            Dextall named as first qualified and approved prefab wall panel
-            provided in sweeping plan to over-clad existing affordable housing
-            projects for the State of New York
+            {{ body.content.description }}
           </p>
           <ul class="advantages__list">
             <li v-for="(item, idx) in li" :key="idx" class="advantages__li">
               <div class="advantages__line"></div>
               <div class="grid advantages__text-wrapper">
                 <p class="advantages__bold-text">
-                  {{ item.boldText }}
+                  {{ item.title_text }}
                 </p>
                 <p class="advantages__italic-text">
-                  {{ item.italicText }}
+                  {{ item.text }}
                 </p>
               </div>
               <div v-if="idx + 1 === li.length" class="advantages__line"></div>
@@ -43,7 +50,7 @@ const li = [
         </div>
         <img
           class="advantages__img"
-          src="/images/advantages-list/1.jpg"
+          :src="body.content.image.filename"
           alt="Image"
         />
       </div>

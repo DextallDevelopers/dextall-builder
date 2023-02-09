b20bcecf2dbeb5fbb3356711dcab75499fe58d42
letmewin22 <lkosteckiy5@gmail.com>
2023-02-09 17:52:13 +0200

    fix: update auth property (again)

--- composables/appState.ts
+++ composables/appState.ts
@@ -2,7 +2,7 @@ export const useAppState = () => {
   const isInEditor = useState('inEditor', () => false)
   const isLoaded = useState('inLoaded', () => false)
   const isWaiting = useState('isWaiting', () => false)
-  const isAuth = useState('isAuth', () => true)
+  const isAuth = useState('isAuth', () => false)
 
   return { isInEditor, isLoaded, isWaiting, isAuth }
 }
